b8e3f45eb6b495296c0045310db097b302039aa8
S3841647 <s3841647@student.rmit.edu.au>
2020-10-16 22:54:56 +1100

    Initial commit

---
+++ app/src/main/java/com/albert/multi_language_week10_application/MainActivity.java
@@ -0,0 +1,18 @@
+package com.albert.multi_language_week10_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //getActionBar().hide();
+    }
+}
